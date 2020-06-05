271e6260309dd77ac0b61f10ea68394ae7901d1f
tangzhi <3297364807@qq.com>
2020-06-05 16:13:36 +0800

    邮件提醒完成

--- app/src/main/java/com/example/test2/Http_tools.java
+++ app/src/main/java/com/example/test2/Http_tools.java
@@ -1,8 +1,7 @@
 package com.example.test2;
-
-import android.os.SystemClock;
-import android.util.Log;
-
+import android.app.ProgressDialog;
+import android.os.Handler;
+import android.widget.Toast;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -10,7 +9,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
-
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Session;
@@ -18,21 +16,19 @@ import javax.mail.Transport;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
-public class Http_tools  {
+public class Http_tools {
     private static final String TAG = "Http_tools";
-
-    public String get(String cookie,String id) throws IOException {
+    public String get(String cookie, String id) throws IOException {
         OkHttpClient client = new OkHttpClient().newBuilder()
                 .build();
         MediaType mediaType = MediaType.parse("application/json");
-        RequestBody body = RequestBody.create(mediaType, "{\"type\":\"org\",\"identity\":\"\",\"para\":{\"organization_id\":\""+id+"\",\"organization_path_str\":\"1649,1650,1651,1652,1653,1654,1655\"},\"date\":\"2020-06-05\",\"activityid\":\"3195\",\"flag\":0,\"domain\":\"cqcfe\",\"stucode\":\"032018030186\"}");
+        RequestBody body = RequestBody.create(mediaType, "{\"type\":\"org\",\"identity\":\"\",\"para\":{\"organization_id\":\"" + id + "\",\"organization_path_str\":\"1649,1650,1651,1652,1653,1654,1655\"},\"date\":\"2020-06-05\",\"activityid\":\"3195\",\"flag\":0,\"domain\":\"cqcfe\",\"stucode\":\"032018030186\"}");
         Request request = new Request.Builder()
                 .url("https://lightapp.weishao.com.cn/api/reportstatistics/reportstatistics/getStatistical")
                 .method("POST", body)
@@ -48,50 +44,71 @@ public class Http_tools  {
 
     public String[] mysql() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.jdbc.Driver");
-        Connection connection= DriverManager.getConnection("jdbc:mysql://39.106.133.87:3306/meigui", "root", "admin");
-        String sql="select * from name_qq";
-        Statement statement=connection.createStatement();
-        ResultSet resultSet=statement.executeQuery(sql);
-        String[] data=new String[1000];
-        int i=0;
-        if(resultSet.next()){
-            data[i]=resultSet.getString("name")+","+resultSet.getString("qq");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://39.106.133.87:3306/meigui", "root", "admin");
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select * from name_qq");
+        String[] data = new String[1000];
+        int i = 0;
+        while (resultSet.next()) {
+            if (i == 0) {
+                data[i] = resultSet.getString("name") + "," + resultSet.getString("qq") + "," + resultSet.getString("cookie");
+            } else {
+                data[i] = resultSet.getString("name") + "," + resultSet.getString("qq");
+            }
             i++;
         }
+        connection.close();
         return data;
     }
 
-    public  void QQMail_Send(String QQ) throws AddressException, MessagingException {
-        Properties properties = new Properties();
-        properties.put("mail.transport.protocol", "smtp");// 连接协议
-        properties.put("mail.smtp.host", "smtp.qq.com");// 主机名
-        properties.put("mail.smtp.port", 465);// 端口号
-        properties.put("mail.smtp.auth", "true");
-        properties.put("mail.smtp.ssl.enable", "true");// 设置是否使用ssl安全连接 ---一般都使用
-        properties.put("mail.debug", "true");// 设置是否显示debug信息 true 会在控制台显示相关信息
+    public void QQMail_Send(InternetAddress[] addresses, MainActivity mainActivity, Handler handler, ProgressDialog progressDialog) throws AddressException, MessagingException {
+        int count = 0;
+        for (int i = 0; i < addresses.length; i++) {//计算非空值数量
+            if (addresses[i] != null) {
+                count++;
+            }
+        }
+        InternetAddress[] add = new InternetAddress[count];
+        for (int i = 0; i < add.length; i++) {//重新赋值
+            add[i] = addresses[i];
+        }
+        if(add.length>0){
+            Properties properties = new Properties();
+            properties.put("mail.transport.protocol", "smtp");// 连接协议
+            properties.put("mail.smtp.host", "smtp.qq.com");// 主机名
+            properties.put("mail.smtp.port", 465);// 端口号
+            properties.put("mail.smtp.auth", "true");
+            properties.put("mail.smtp.ssl.enable", "true");// 设置是否使用ssl安全连接 ---一般都使用
+            properties.put("mail.debug", "true");// 设置是否显示debug信息 true 会在控制台显示相关信息
 // 得到回话对象
-        Session session = Session.getInstance(properties);
+            Session session = Session.getInstance(properties);
 // 获取邮件对象
-        Message message = new MimeMessage(session);
+            Message message = new MimeMessage(session);
 // 设置发件人邮箱地址
-        message.setFrom(new InternetAddress("3297364807@qq.com"));
-// 设置收件人邮箱地址
-//        InternetAddress[] Add = new InternetAddress[1];
+            message.setFrom(new InternetAddress("3297364807@qq.com"));
 //        Add[0] = new InternetAddress("1414394854@qq.com");
-        message.setRecipient(Message.RecipientType.TO, new InternetAddress(""+QQ+"@qq.com"));//单个收件人
-//        message.setRecipients();  s是数组
+//        message.setRecipient(Message.RecipientType.TO, new InternetAddress(""+QQ+"@qq.com"));//单个收件人
+            message.setRecipients(Message.RecipientType.TO, add);  //dd多个收件人
 // 设置邮件标题
-        message.setSubject("唐智：微哨打卡");
+            message.setSubject("唐智：微哨打卡");
 // 设置邮件内容
-        message.setText("亲爱的同学你的微哨未打卡，请尽快打卡哟");
+            message.setText("亲爱的同学你的微哨未打卡，请尽快打卡哟！来自3297364807的提醒");
 // 得到邮差对象
-        Transport transport = session.getTransport();
+            Transport transport = session.getTransport();
 // 连接自己的邮箱账户
-        transport.connect("3297364807@qq.com", "cvxidvggrrehchdd");// 密码为QQ邮箱开通的stmp服务后得到的客户端授权码
+            transport.connect("3297364807@qq.com", "cvxidvggrrehchdd");// 密码为QQ邮箱开通的stmp服务后得到的客户端授权码
 // 发送邮件
-        transport.sendMessage(message, message.getAllRecipients());
-        transport.close();
+            transport.sendMessage(message, message.getAllRecipients());
+            transport.close();
+            handler.post(()-> {
+                progressDialog.dismiss();
+                Toast.makeText(mainActivity, "发送完毕", Toast.LENGTH_SHORT).show();
+            });
+        }else {
+            handler.post(()->{
+                progressDialog.dismiss();
+                Toast.makeText(mainActivity ,"全班已打卡完成", Toast.LENGTH_SHORT).show();
+            });
+        }
     }
-
-
 }
--- app/src/main/java/com/example/test2/MainActivity.java
+++ app/src/main/java/com/example/test2/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.test2;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.os.SystemClock;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import org.json.JSONException;
@@ -21,33 +29,52 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private TextView tv1, tv2, tv3, tv4, tv5, tv6, tv7;
+    private AlertDialog dialog;
+    private String number=null;
+    private ProgressDialog progressDialog;
+    private Handler handler=new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initShow();
+        initCheck();
+    }
+    private void init_Request(String Class_name) {
         new Thread(() -> {
             Http_tools tools = new Http_tools();
             try {
                 String[] data = tools.mysql();
-
-                String cookie = "[{\"key\":\"Cookie\",\"value\":\"whistle-spaserver=s%3A_b2ZastbhP5EIfyamKc0iQZyc4uWLp-Q.y%2FiP1kj00cYmXLvX1zKx2FP2zab4kKgy9PdylVFPnzs;\",\"description\":\"\",\"type\":\"text\",\"enabled\":true}]";
-                JSONObject json = new JSONObject(tools.get(cookie, "1649"));
-                for(int i=0;i<json.getJSONObject("data").getJSONArray("users").length();i++){
+                String cookie = " whistle-spaserver=s%3AqFAmGsznuyGhvNxiPCaO7Nm6-blSN-Rz.yT88tDPh1Xop7BN4iy%2FN4ufULhn2W423%2FFyN1a6wbXI; ";
+                JSONObject json = new JSONObject(tools.get(cookie, Class_name));
+                int len = json.getJSONObject("data").getJSONArray("users").length();
+                InternetAddress[] address = new InternetAddress[len];
+                int count = 0;
+                for (int i = 0; i < len; i++) {
                     String name = json.getJSONObject("data").getJSONArray("users").getJSONObject(i).getString("user_name");
                     int report = json.getJSONObject("data").getJSONArray("users").getJSONObject(i).getInt("is_report");
                     if (report == 1) {
                         System.out.println(name + "：已打卡");
                     } else {
-                        System.out.println(name + "：未打卡");
-                        for(int j=0;j<data.length;j++){
-                            String[] qq_name=data[j].split(",");//qq_name[0]是姓名  [1]是qq号
-                            if(name.equals(qq_name[0])){//找到姓名
-
-
+                        Log.e(TAG, name + "：未打卡");
+                        for (int j = 0; j < data.length; j++) {//找到对应的名字
+                            if (data[j] != null) {
+                                String[] qq_name = data[j].split(",");//qq_name[0]is name  [1]is QQ NO
+                                System.out.println(data[j] + ">>>>>");
+                                if (name.equals(qq_name[0])) {//找到姓名
+                                    address[count] = new InternetAddress(qq_name[1] + "@qq.com");
+                                    count++;
+                                }
                             }
                         }
                     }
                 }
+                //start request
+
+                tools.QQMail_Send(address,this,handler,progressDialog);
+
             } catch (SQLException e) {
                 e.printStackTrace();
             } catch (ClassNotFoundException e) {
@@ -56,9 +83,76 @@ public class MainActivity extends AppCompatActivity {
                 e.printStackTrace();
             } catch (JSONException e) {
                 e.printStackTrace();
+            } catch (AddressException e) {
+                e.printStackTrace();
+            } catch (MessagingException e) {
+                e.printStackTrace();
             }
 //                QQMail_Send();
         }).start();
+    }
+
+    private void initCheck() {
+        tv1.setOnClickListener(v -> {
+            number="1649";
+            dialog.show();
+        });
+        tv2.setOnClickListener(v -> {
+            number="1650";
+            dialog.show();
+        });
+        tv3.setOnClickListener(v -> {
+            number="1651";
+            dialog.show();
+        });
+        tv4.setOnClickListener(v -> {
+            number="1652";
+            dialog.show();
+        });
+        tv5.setOnClickListener(v -> {
+            number="1653";
+            dialog.show();
+        });
+        tv6.setOnClickListener(v -> {
+            number="1654";
+            dialog.show();
+        });
+        tv7.setOnClickListener(v -> {
+            number="1655";
+            dialog.show();
+        });
+    }
+
+    private void initShow() {
+        tv1 = findViewById(R.id.btn1);
+        tv2 = findViewById(R.id.btn2);
+        tv3 = findViewById(R.id.btn3);
+        tv4 = findViewById(R.id.btn4);
+        tv5 = findViewById(R.id.btn5);
+        tv6 = findViewById(R.id.btn6);
+        tv7 = findViewById(R.id.btn7);
+        progressDialog=new ProgressDialog(this);
+        progressDialog.setTitle("微哨打卡");
+        progressDialog.setMessage("加载中....");
+        progressDialog.setCancelable(false);
+        dialog = new AlertDialog.Builder(this).setTitle("确定提醒吗").setCancelable(false)
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if(number!=null){
+                            progressDialog.show();
+                            init_Request(number);
+                        }else {
+                            Toast.makeText(MainActivity.this, "班级为空的", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                })
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                }).create();
     }
 
 
